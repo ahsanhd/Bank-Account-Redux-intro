4da3438e27c15de2e9d86179ff3a8b983c69c476
Ahsan <ahsanhd@outlook.com>
2023-11-09 06:00:00 +0500

    feautre: Provider of react-redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import store from "./store";
+import { Provider } from "react-redux";
 
 // store.dispatch({ type: "account/withdraw", payload: 200 });
 // console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
