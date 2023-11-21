efaf13225b6ec0e050e3ea844d18c39b119afd6e
Ahsan <ahsanhd@outlook.com>
2023-11-21 23:22:36 +0500

    feautre: setup store with configureStore

--- src/store.js
+++ src/store.js
@@ -1,14 +1,12 @@
-import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-const root = combineReducers({
-  account: accountReducer,
-  customer: customerReducer,
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
 });
-
-const store = createStore(root, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
